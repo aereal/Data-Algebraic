492a82061f1c754cfef09aa96221fd2398ebdb56
aereal <aereal@aereal.org>
2015-07-25 00:55:12 +0900

    Refactor: $from -> $ordinal

--- lib/Data/Enum/Installer/Numeral.pm
+++ lib/Data/Enum/Installer/Numeral.pm
@@ -10,12 +10,12 @@ Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'from');
 sub define_values {
   my ($self) = @_;
 
-  my $from = $self->from // 1;
+  my $ordinal = $self->from // 1;
   for my $name (@{ $self->names }) {
-    my $v = $self->create_value(name => $name, value => $from);
+    my $v = $self->create_value(name => $name, value => $ordinal);
     $self->define_value($v);
     Data::Enum::Util::Accessor::define_sub($self->entity_class, $name, sub { $v });
-    $from++;
+    $ordinal++;
   }
 }
 

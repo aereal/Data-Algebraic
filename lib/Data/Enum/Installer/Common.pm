d41504f89f2dd4ed9b9d2f45c2e1ee1e70bb813e
aereal <aereal@aereal.org>
2015-07-25 00:44:54 +0900

    Extract common implementations to D::E::Installer::Common

---
+++ lib/Data/Enum/Installer/Common.pm
@@ -0,0 +1,52 @@
+package Data::Enum::Installer::Common;
+use strict;
+use warnings;
+
+use Data::Enum::Util::Accessor;
+
+Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'names');
+Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'entity_class');
+
+sub parse {
+  my ($class, @args) = @_;
+
+  my $options = {};
+  while (@args) {
+    my $k = $args[0];
+    last unless $k =~ s/\A-//;
+    shift @args;
+    my $v = shift @args;
+    $options->{$k} = $v;
+  }
+  my $names = \@args;
+
+  my $self = bless {
+    names => $names,
+    %$options,
+  }, $class;
+  return $self;
+}
+
+sub install {
+  my ($self) = @_;
+
+  Data::Enum::Util::Accessor::define_ro_accessor($self->entity_class, 'name');
+  Data::Enum::Util::Accessor::define_ro_accessor($self->entity_class, 'value');
+
+  my $values = $self->define_values();
+  my $values_by_raw_value = { map { ($_->value => $_) } @$values };
+
+  Data::Enum::Util::Accessor::define_sub($self->entity_class, 'is', sub {
+    my ($self, $other) = @_;
+    $self->value == $other->value;
+  });
+
+  Data::Enum::Util::Accessor::define_sub($self->entity_class, 'from', sub {
+    my ($class, $value) = @_;
+    $values_by_raw_value->{$value};
+  });
+
+  Data::Enum::Util::Accessor::define_scalar_var($self->entity_class, 'VALUES', $values);
+}
+
+1;
--- lib/Data/Enum/Installer/Numeral.pm
+++ lib/Data/Enum/Installer/Numeral.pm
@@ -1,61 +1,27 @@
 package Data::Enum::Installer::Numeral;
 use strict;
 use warnings;
+use parent qw( Data::Enum::Installer::Common );
 
 use Data::Enum::Util::Accessor;
 
-Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'names');
 Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'from');
-Data::Enum::Util::Accessor::define_ro_accessor(__PACKAGE__, 'entity_class');
 
-sub parse {
-  my ($class, @args) = @_;
-
-  my $options = {};
-  while (@args) {
-    my $k = $args[0];
-    last unless $k =~ s/\A-//;
-    shift @args;
-    my $v = shift @args;
-    $options->{$k} = $v;
-  }
-  my $names = \@args;
-
-  my $self = bless {
-    names => $names,
-    %$options,
-  }, $class;
-  return $self;
-}
-
-sub install {
+sub define_values {
   my ($self) = @_;
 
-  Data::Enum::Util::Accessor::define_ro_accessor($self->entity_class, 'name');
-  Data::Enum::Util::Accessor::define_ro_accessor($self->entity_class, 'value');
-
+  my @values;
   my $from = $self->from // 1;
-  my $values_by_name = {};
   for my $name (@{ $self->names }) {
     my $v = $self->create_value(name => $name, value => $from);
-    $values_by_name->{$v->value} = $v;
+    push @values, $v;
     Data::Enum::Util::Accessor::define_sub($self->entity_class, $name, sub { $v });
     my $predicate_name = sprintf 'is_%s', lc $name;
     Data::Enum::Util::Accessor::define_sub($self->entity_class, $predicate_name, sub { $_[0]->is($v) });
     $from++;
   }
 
-  Data::Enum::Util::Accessor::define_sub($self->entity_class, 'is', sub {
-    my ($self, $other) = @_;
-    $self->value == $other->value;
-  });
-
-  Data::Enum::Util::Accessor::define_sub($self->entity_class, 'from', sub {
-    my ($class, $value) = @_;
-    $values_by_name->{$value};
-  });
-
-  Data::Enum::Util::Accessor::define_scalar_var($self->entity_class, 'VALUES', [ values %$values_by_name ]);
+  return \@values;
 }
 
 sub create_value {
